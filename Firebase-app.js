05f5227de567a2058f01a731c33fa6a663856f68
Tanvanat <tanvanat112@gmail.com>
2025-08-30 19:25:03 +0700

    nearly final

--- Firebase-app.js
+++ Firebase-app.js
@@ -1,25 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDOkC8aU2xjZSKdplAlTjo6I0YAs-1pBqM",
-//   authDomain: "form-test-5906d.firebaseapp.com",
-//   projectId: "form-test-5906d",
-//   storageBucket: "form-test-5906d.firebasestorage.app",
-//   messagingSenderId: "586975002404",
-//   appId: "1:586975002404:web:ce41b7957996c73f0cfdb1",
-//   measurementId: "G-DZ2RNLCY4G"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 // Firebase-app.js (ใช้ ES Module)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
